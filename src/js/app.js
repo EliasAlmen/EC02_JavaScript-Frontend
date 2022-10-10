3ee794ca2b6b7f9b6f153ea307b009186ec2fd4f
unknown <almen.elias@gmail.com>
2022-10-10 14:39:47 +0200

    Contact page added

--- src/js/app.js
+++ src/js/app.js
@@ -16,14 +16,32 @@
 // });
 
 
-function knapp() {
-    var btn = document.getElementsByClassName("button");
-    for (i = 0; i < btn.length; i++) {
-      btn[i].addEventListener("click", function() {
-      console.log("you clicked"); });
-    }
-  }
+// function knapp() {
+//     var btn = document.getElementsByClassName("button");
+//     for (i = 0; i < btn.length; i++) {
+//       btn[i].addEventListener("click", function() {
+//       console.log("you clicked"); });
+//     }
+//   }
   
-  window.addEventListener("load",function() {
-    knapp();
-  });
+//   window.addEventListener("load",function() {
+//     knapp();
+//   });
+
+// // Initialize and add the map
+// function initMap() {
+//     // The location of Uluru
+//     const uluru = { lat: -25.344, lng: 131.031 };
+//     // The map, centered at Uluru
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//       zoom: 4,
+//       center: uluru,
+//     });
+//     // The marker, positioned at Uluru
+//     const marker = new google.maps.Marker({
+//       position: uluru,
+//       map: map,
+//     });
+//   }
+  
+//   window.initMap = initMap;
