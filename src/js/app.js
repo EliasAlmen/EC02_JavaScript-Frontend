c2b625a18dfea66f78e0b6115ca393569f4e9ee3
unknown <almen.elias@gmail.com>
2022-10-12 14:13:09 +0200

    Form now has feedback on keyUp

--- src/js/app.js
+++ src/js/app.js
@@ -1,75 +1,72 @@
-// console.log(document.getElementsByClassName("button"))
-
-// let a = document.getElementsByClassName("button")
-
-// for (let index = 0; index < a.length; index++) {
-//     const element = a[index];
-//     addEventListener("click", function(){console.log("hej")});
-// }
-
-// a.forEach(a => {
-//     addEventListener("click", displayDate);
-// });
-
-// a.forEach(button => {
-//     addEventListener("click", displayDate);
-// });
-
-
-// function knapp() {
-//     var btn = document.getElementsByClassName("button");
-//     for (i = 0; i < btn.length; i++) {
-//       btn[i].addEventListener("click", function() {
-//       console.log("you clicked"); });
-//     }
-//   }
-  
-//   window.addEventListener("load",function() {
-//     knapp();
-//   });
-
-// document.getElementById('contact_form').addEventListener('submit', (e) =>{
-//     e.preventDefault()
-//     console.log(e);
-// })
 'use strict';
+
 const onSubmit = event => {
     event.preventDefault()
-    for (let element of event.target)
-        if (element.required) {
-            let label = document.getElementById(`${element.id}-label`).innerText
-            let error = ""
-            switch(element.type) {
-                case 'text':
-                    if (!isNullOrEmpty(element.value)) {
-                        if (!isMinLength(element.value, element.dataset.requiredMin)) {
-                            error = `Your ${label.toLocaleLowerCase()} must contain at least ${element.dataset.requiredMin} letters.`
-                        }
-                    } else {
-                        error = `You must enter a ${label.toLocaleLowerCase()}`
+    validate(event)
+}
+
+const validate = (event) => {
+    switch(event.type) {
+        case 'keyup':
+            validateElement(event.target)
+            break;
+
+        case 'submit':
+            for(let element of event.target)
+                validateElement(element)
+            break;
+    }
+}
+
+const validateElement = (element) => {
+    if (element.required) {
+        let label = document.getElementById(`${element.id}-label`).innerText
+        let error = ""
+        let success = ""
+        switch(element.type) {
+            case 'text':
+                if (!isNullOrEmpty(element.value)) {
+                    if (!isMinLength(element.value, element.dataset.requiredMin)) {
+                        error = `Your ${label.toLocaleLowerCase()} must contain at least ${element.dataset.requiredMin} letters.`
                     }
-                    break;
-                case 'email':
-                    if (!isNullOrEmpty(element.value)) {
-                        if (!isMinLength(element.value, element.dataset.requiredMin)) {
-                            error = `Your ${label.toLocaleLowerCase()} must contain at least ${element.dataset.requiredMin} letters.`
-                        }
-                    } else {
-                        error = `You must enter an ${label.toLocaleLowerCase()}`
+                    if (!isNameValid(element.value)) {
+                        error = `Your ${label.toLocaleLowerCase()} cannot contain numbers`
                     }
-                    break;
-                case 'textarea':
-                    if (!isNullOrEmpty(element.value)) {
-                        if (!isMinLength(element.value, element.dataset.requiredMin)) {
-                            error = `Your ${label.toLocaleLowerCase()} must contain at least ${element.dataset.requiredMin} letters.`
-                        }
-                    } else {
-                        error = `You must enter a ${label.toLocaleLowerCase()}`
+                    if (isMinLength(element.value, element.dataset.requiredMin) && isNameValid(element.value)) {
+                        success = `Looks good!`
                     }
-                    break;
-            }
-            document.getElementById(`${element.id}-error`).innerText = error
+                } else {
+                    error = `You must enter a ${label.toLocaleLowerCase()}`
+                }
+                break;
+            case 'email':
+                if (!isNullOrEmpty(element.value)) {
+                    if (!isEmailValid(element.value)) {
+                        error = `Your ${label.toLocaleLowerCase()} must be valid. (eg. example@domain.com)`
+                    }
+                    if (isMinLength(element.value, element.dataset.requiredMin) && isEmailValid(element.value)) {
+                        success = `Looks good!`
+                    }
+                } else {
+                    error = `You must enter an ${label.toLocaleLowerCase()}`
+                }
+                break;
+            case 'textarea':
+                if (!isNullOrEmpty(element.value)) {
+                    if (!isMinLength(element.value, element.dataset.requiredMin)) {
+                        error = `Your ${label.toLocaleLowerCase()} must contain at least ${element.dataset.requiredMin} letters.`
+                    }
+                    if (isMinLength(element.value, element.dataset.requiredMin)) {
+                        success = `Looks good!`
+                    }
+                } else {
+                    error = `You must enter a ${label.toLocaleLowerCase()}`
+                }
+                break;
         }
+        document.getElementById(`${element.id}-error`).innerText = error
+        document.getElementById(`${element.id}-success`).innerText = success
+    }
 }
 
 const isNullOrEmpty = value =>{
@@ -88,4 +85,15 @@ const isMinLength = (value, minLength = 2) =>{
 
 const isEmailValid = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (regEx.test(email))
+        return true
+    
+    return false
+}
+
+const isNameValid = (name) => {
+    const regEx = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+    if (regEx.test(name))
+        return true
+    return false
 }
