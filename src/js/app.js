09470661fc50b8cf9725dd6f3cb6d621f1d7bae2
unknown <almen.elias@gmail.com>
2022-10-12 14:32:46 +0200

    Trying to validate whitespace

--- src/js/app.js
+++ src/js/app.js
@@ -32,6 +32,9 @@ const validateElement = (element) => {
                     if (!isNameValid(element.value)) {
                         error = `Your ${label.toLocaleLowerCase()} cannot contain numbers`
                     }
+                    // if (!removeWhiteSpace(element.value)) {
+                    //     error = `Your ${label.toLocaleLowerCase()} cannot start with whitespace`
+                    // } && removeWhiteSpace(element.value)
                     if (isMinLength(element.value, element.dataset.requiredMin) && isNameValid(element.value)) {
                         success = `Looks good!`
                     }
@@ -82,6 +85,13 @@ const isMinLength = (value, minLength = 2) =>{
 
     return false
 }
+
+// const removeWhiteSpace = value => {
+//     if (value.trim())
+//         return true
+
+//     return false
+// }
 
 const isEmailValid = (email) => {
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
